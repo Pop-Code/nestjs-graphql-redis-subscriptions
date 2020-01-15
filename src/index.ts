5067ecf3ed535d0f5d8186ed6b68e8107ce5eb35
Alex Hermann <alex.hermann@pop-code.com>
2020-01-15 03:08:04 +0100

    Fix exports

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ export * from './module';
 export * from './module.core';
 export * from './constants';
 export * from './helpers';
+export { RedisPubSub } from 'graphql-redis-subscriptions';
+export { Redis } from 'ioredis';
